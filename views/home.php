24ac36221831fc9891e77b80ca51c94ca24ae5d9
Robloblo <108494251+Robloblo@users.noreply.github.com>
2024-02-17 17:53:04 +0100

    oui c'est bon je push

--- views/home.php
+++ views/home.php
@@ -4,8 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="../public/css/header.css" rel="stylesheet" />
+    <link href="../public/css/base.css" rel="stylesheet" />
+    <script defer src="../public/js/header.js"></script>
 </head>
 <body>
-    <h1>TEST</h1>
+    <!--  Partie Header  -->
+    <header> 
+        <nav class="navigation">
+            <div id="menuToggle">
+
+                <input type="checkbox" />
+                
+
+                <span></span>
+                <span></span>
+                <span></span>
+
+                <ul class="cacher">
+                <a href="#"><li>Playlist</li></a>
+                <a href="#"><li>Albums</li></a>
+                <a href="#"><li>Artistes</li></a>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="WrapperSearchBar">
+            <form action="">
+                <input class="searchBar" type="text">
+                <input class="btnSearch" type="submit" value="">
+            </form>
+        </div>
+        
+        <div class="connection">
+            <button> Se connecter</button>
+        </div>
+
+    </header>
+    <!-- Fin partie Header  -->
+
+
 </body>
 </html>
