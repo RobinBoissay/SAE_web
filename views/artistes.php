b15dca6e360b88a3e0be9288057325430fa5fd08
Robloblo <108494251+Robloblo@users.noreply.github.com>
2024-02-18 01:39:00 +0100

    css page artistes

--- views/artistes.php
+++ views/artistes.php
@@ -1,12 +1,23 @@
-<?php
+<link rel="stylesheet" href="../css/artistes.css">
+<section class="wrapperSectionArtiste">
+    <?php
+        
+        
+        foreach($artistes as $artiste){
+            echo '<div>';
+            echo '<h2>'.$artiste->getNom().'</h2>';
+            echo '<div>';
+            foreach($albumsArtiste[$artiste->getId()] as $album){
+                echo '<div>';
+                echo '<img src="img/'.$album->getImage().'" alt="image de l\'album '.$album->getTitre().'">';
+                echo '<h3>'.$album->getTitre().'</h3>';
+                echo '<p>'.$album->getAnnee().'</p>';
+                echo '</div>';
 
-    foreach($artistes as $artiste){
-        echo '<h2>'.$artiste->getNom().'</h2>';
-        foreach($albumsArtiste[$artiste->getId()] as $album){
-            echo '<img src="img/'.$album->getImage().'" alt="image de l\'album '.$album->getTitre().'">';
-            echo '<h3>'.$album->getTitre().'</h3>';
-            echo '<p>'.$album->getAnnee().'</p>';
+            }
+            echo '</div>';
+            echo '</div>';
         }
-    }
 
-?>
+    ?>
+</section>
